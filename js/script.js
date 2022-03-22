304f7a8b0b07cd2172cd7d54d619945e1e38be9c
Yan <yan.bocharovv@gmail.com>
2022-03-22 23:11:21 +0300

    Добавил дни в таймер

--- js/script.js
+++ js/script.js
@@ -37,17 +37,18 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //Timer 
 
-    let deadline = '2022-12-21';
+    let deadline = '2022-3-23';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
         seconds = Math.floor((t/1000) % 60),
         minutes = Math.floor((t/1000/60) % 60),
-        hours = Math.floor((t/( 1000*60*60)));
-        // days = Math.floor((t/(1000*60*60*24)))
+        hours = Math.floor((t/(1000*60*60)%60)),
+        days = Math.floor((t/(1000*60*60*24)));
 
         return {
             'total' : t,
+            'days' : days,
             'hours' : hours,
             'minutes' : minutes,
             'seconds' : seconds
@@ -56,6 +57,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     function setClock(id, endtime) {
         let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
@@ -63,12 +65,24 @@ window.addEventListener('DOMContentLoaded', function() {
 
         function updateClock() {
             let t = getTimeRemaining(endtime);
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
+
+            function addZero(num){
+                if(num <= 9) {
+                    return '0' + num;
+                } else return num;
+            };
+
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
             }
         }
     }
